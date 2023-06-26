b90b1e67a861edc35a5a08692c08dd463f690625
PriyankaSahu02 <135947084+PriyankaSahu02@users.noreply.github.com>
2023-06-26 18:34:12 +0530

    Add files via upload

---
+++ Most frequent.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+@author: PriyankaSahu
+# Python Program to create most_frequent 
+ 
+"""
+abc = input('Please enter a string ')
+def most_frequent(string):
+    d = dict()
+    for key in string:
+        if key not in d:
+            d[key] = 1
+        else:
+            d[key] += 1
+    return d
+
+print (most_frequent(abc))
+         
